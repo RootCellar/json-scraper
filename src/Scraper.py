cd7caa4738d5562146d316ef7494d7e42639900a
RootCellar <RootCellar9877@gmail.com>
2023-11-03 16:12:24 -0800

    write down expected usage of Scraper once it is implemented

--- src/Scraper.py
+++ src/Scraper.py
@@ -2,3 +2,14 @@
 # Author: Darian Marvel
 #
 #
+
+# PSEUDOCODE Usage
+#
+# Scraper scrappy
+# scrappy.thenSkipToClass( ".something-after-the-header" )
+# scrappy.thenSkipToElement( "p" )
+# scrappy.thenSelectElement( "p" ) # This would be the first "p" element after the one we just skipped to
+# scrappy.thenSaveValueAsProperty( "employee.name" )
+# scrappy.thenScrapeTable( "employee.contactInfo" )
+#
+# JsonData data = scrappy.scrape( HTML_Feeder_or_something )
