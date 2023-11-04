cc327d1e2ab286c32b2c6c12b4be819d3e6bfb3a
RootCellar <RootCellar9877@gmail.com>
2023-11-03 20:41:39 -0800

    create class scraper with unfinished methods

--- src/Scraper.py
+++ src/Scraper.py
@@ -13,3 +13,34 @@
 # scrappy.thenScrapeTable( "employee.contactInfo" )
 #
 # JsonData data = scrappy.scrape( HTML_Feeder_or_something )
+
+class Scraper(object):
+
+    def __init__(self):
+        self.instructions = []
+        pass
+
+    def getInstructions(self):
+        return self.instructions
+
+    def thenSkipToClass(self, param):
+        self.instructions.append(param)
+        pass
+
+    def thenSkipToElement(self, param):
+        self.instructions.append(param)
+        pass
+
+    def thenSelectElement(self, param):
+        self.instructions.append(param)
+        pass
+
+    def thenSaveValueAsProperty(self, param):
+        self.instructions.append(param)
+        pass
+
+    def thenScrapeTable(self, param):
+        self.instructions.append(param)
+        pass
+
+
