a55cf9ad79490730820bacc48d63a946e802446e
RootCellar <RootCellar9877@gmail.com>
2023-11-03 20:42:44 -0800

    write code to test scraper functionality as it is developed

--- src/Extract.py
+++ src/Extract.py
@@ -10,3 +10,13 @@ import Scraper
 if __name__ == "__main__":
     print("Hello, World!")
 
+    # TEMPORARY FOR TESTING
+    scrappy = Scraper.Scraper()
+    scrappy.thenSkipToClass( ".something-after-the-header" )
+    scrappy.thenSkipToElement( "p" )
+    scrappy.thenSelectElement( "p" ) # This would be the first "p" element after the one we just skipped to
+    scrappy.thenSaveValueAsProperty( "employee.name" )
+    scrappy.thenScrapeTable( "employee.contactInfo" )
+
+    print(scrappy.getInstructions())
+
