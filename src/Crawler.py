2a49c86a02c4c1f4c00678859e52902b350cc99c
RootCellar <RootCellar9877@gmail.com>
2023-11-03 22:39:33 -0800

    begin methods for Crawler

--- src/Crawler.py
+++ src/Crawler.py
@@ -3,7 +3,25 @@
 #
 #
 
+import Debug
+
 class Crawler(object):
 
     def __init__(self):
-        pass
+        self.instructions = []
+
+    @staticmethod
+    def __debug(param):
+        Debug.debug("[CRAWLER] " + param)
+
+    def get_instructions(self):
+        return self.instructions
+
+    def add_instruction(self, param):
+        self.__debug("Adding instruction \"" + param + "\"")
+        self.instructions.append(param)
+
+    def crawl(self):
+        self.__debug("Crawling...")
+        for instruction in self.instructions:
+            self.__debug(instruction)
