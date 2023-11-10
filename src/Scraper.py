7f700bf603ff0b773276368c0149785ad5b048c3
RootCellar <RootCellar9877@gmail.com>
2023-11-09 15:40:15 -0900

    perform element locating and instruction execution in class Scraper

--- src/Scraper.py
+++ src/Scraper.py
@@ -3,6 +3,7 @@
 #
 #
 
+
 # PSEUDOCODE Usage
 #
 # Scraper scrappy
@@ -14,46 +15,82 @@
 #
 # JsonData data = scrappy.scrape( HTML_Feeder_or_something )
 
+from selenium.webdriver.common.by import By
+
 import Debug
+from InstructionType import InstructionType
 
 
 class Scraper(object):
 
     def __init__(self):
         self.instructions = []
+        self.current_element = None
 
     @staticmethod
     def __debug(param):
-        Debug.debug("[SCRAPER] " + param)
+        Debug.debug("[SCRAPER] " + param.__str__())
+
+    def is_after(self, element, elem):
+        if elem.location.get('y') < element.location.get('y'):
+            return False
+        if elem.location.get('y') > element.location.get('y'):
+            return True
+        if elem.location.get('x') > element.location.get('x'):
+            return True
+
+    def is_before(self, element, elem):
+        if elem.location.get('y') > element.location.get('y'):
+            return False
+        if elem.location.get('y') < element.location.get('y'):
+            return True
+        if elem.location.get('x') < element.location.get('x'):
+            return True
+
+    def next_closest_element(self, elems):
+        self.__debug("next_closest_element")
+        closest = self.current_element
+        for elem in elems:
+            if self.is_after(self.current_element, elem):
+                closest = elem
+        for elem in elems:
+            if self.is_after(self.current_element, elem) and self.is_before(closest, elem):
+                closest = elem
+        return closest
 
     def get_instructions(self):
         return self.instructions
 
     def add_instruction(self, param):
-        self.__debug("Adding instruction \"" + param + "\"")
+        self.__debug("Adding instruction \"" + param.__str__() + "\"")
         self.instructions.append(param)
 
     def then_skip_to_class(self, param):
-        self.add_instruction(param)
-        pass
+        instruction = [InstructionType.skip_to_class, param]
+        self.add_instruction(instruction)
 
     def then_skip_to_element(self, param):
-        self.add_instruction(param)
-        pass
+        instruction = [InstructionType.skip_to_tag, param]
+        self.add_instruction(instruction)
 
     def then_select_element(self, param):
-        self.add_instruction(param)
+        # self.add_instruction(param)
         pass
 
     def then_save_value_as_property(self, param):
-        self.add_instruction(param)
+        # self.add_instruction(param)
         pass
 
     def then_scrape_table(self, param):
-        self.add_instruction(param)
+        # self.add_instruction(param)
         pass
 
-    def scrape(self):
+    def scrape(self, webdriver):
         self.__debug("Scraping...")
+        self.current_element = webdriver.find_element(By.TAG_NAME, "html")
         for instruction in self.instructions:
-            self.__debug(instruction)
+            self.__debug("Running instruction: " + instruction.__str__())
+            if instruction[0] is InstructionType.skip_to_class:
+                self.current_element = self.next_closest_element(webdriver.find_elements(By.CLASS_NAME, instruction[1]))
+            if instruction[0] is InstructionType.skip_to_tag:
+                self.current_element = self.next_closest_element(webdriver.find_elements(By.TAG_NAME, instruction[1]))
