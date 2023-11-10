2ae1b9303b514a2da11eac376b9b393295ba87b4
RootCellar <RootCellar9877@gmail.com>
2023-11-09 15:39:18 -0900

    remove element locating code from Extract.py

--- src/Extract.py
+++ src/Extract.py
@@ -12,39 +12,6 @@ import Crawler
 import Scraper
 import Debug
 
-def is_after(element, elem):
-    if elem.location.get('y') < element.location.get('y'):
-        return False
-    if elem.location.get('y') > element.location.get('y'):
-        return True
-    if elem.location.get('x') > element.location.get('x'):
-        return True
-
-def is_before(element, elem):
-    if elem.location.get('y') > element.location.get('y'):
-        return False
-    if elem.location.get('y') < element.location.get('y'):
-        return True
-    if elem.location.get('x') < element.location.get('x'):
-        return True
-
-def next_element_of_tag(webdriver, element, tag):
-    Debug.debug("next_element_of_tag")
-    elems = webdriver.find_elements(By.TAG_NAME, tag)
-    closest = element
-    for elem in elems:
-        if is_after(element, elem):
-            closest = elem
-    #print(element.location)
-    for elem in elems:
-        print(elem.location)
-        print(elem.get_attribute("href"))
-        if is_after(element, elem) and is_before(closest, elem):
-            closest = elem
-            print(closest.location)
-            print(closest.get_attribute("href"))
-    return closest
-
 if __name__ == "__main__":
 
     # TEMPORARY FOR TESTING
@@ -54,31 +21,18 @@ if __name__ == "__main__":
     crawler.crawl()
 
     scrappy = Scraper.Scraper()
-    scrappy.then_skip_to_class(".something-after-the-header")
+    scrappy.then_skip_to_class("something-after-the-header")
     scrappy.then_skip_to_element("p")
     scrappy.then_select_element("p") # This would be the first "p" element after the one we just skipped to
     scrappy.then_save_value_as_property("employee.name")
     scrappy.then_scrape_table("employee.contactInfo")
-
-    scrappy.scrape()
 
     print(scrappy.get_instructions())
 
     driver = webdriver.Firefox()
     driver.get("https://duckduckgo.com")
 
-    elem = driver.find_element(By.NAME, "q")
-    elem.clear()
-    elem.send_keys("Selenium in Python")
-    elem.send_keys(Keys.RETURN)
-    sleep(5)
-
-    elem = driver.find_element(By.ID, "r1-0")
-    elem = next_element_of_tag(driver, elem, "a")
-    elem = next_element_of_tag(driver, elem, "a")
-
-    print(elem.get_attribute("href"))
-    print(elem.location)
+    scrappy.scrape(driver)
 
     driver.close()
 
