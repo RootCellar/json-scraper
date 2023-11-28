075c019965075decd21f9a37511e8090e518a8f4
RootCellar <RootCellar9877@gmail.com>
2023-11-28 14:58:16 -0900

    create other needed instruction types for crawler

--- src/CrawlerInstructionType.py
+++ src/CrawlerInstructionType.py
@@ -7,3 +7,7 @@ from enum import Enum
 
 class CrawlerInstructionType(Enum):
     click_element = 0
+    skip_to_tag = 1
+    skip_to_class = 2
+    skip_to_element_with_attribute = 3
+    goto_previous_page = 4
