da8385aaa68186c0d4890beb6eafe949fdaee095
RootCellar <RootCellar9877@gmail.com>
2023-11-28 15:17:03 -0900

    test crawler

--- src/Extract.py
+++ src/Extract.py
@@ -23,7 +23,6 @@ if __name__ == "__main__":
 
     crawler = Crawler.Crawler()
     ScraperDefinitions.WaterSystem.setup_crawler(crawler)
-    crawler.crawl_and_scrape()
 
     scrappy = Scraper.Scraper()
     ScraperDefinitions.WaterSystem.setup_scraper(scrappy)
@@ -32,12 +31,26 @@ if __name__ == "__main__":
     print(scrappy.get_instructions())
 
     driver = webdriver.Firefox()
-    driver.get("https://dec.alaska.gov/dww/JSP/WaterSystemDetail.jsp?tinwsys_is_number=3708&tinwsys_st_code=AK&wsnumber=AK2310683")
+    # driver.get("https://dec.alaska.gov/dww/JSP/WaterSystemDetail.jsp?tinwsys_is_number=3708&tinwsys_st_code=AK&wsnumber=AK2310683")
+    driver.get("https://dec.alaska.gov/dww/index.jsp")
+
+    # Click the button to navigate to the water system list
+
+    temp_scraper = Scraper.Scraper()
+    temp_scraper.activate_live_mode()
+    temp_scraper.set_web_driver(driver)
+    temp_scraper.then_go_back_to_beginning()
+    temp_scraper.then_skip_to_element_with_attribute("input", "value", "Search For Water Systems")
+    temp_scraper.current_element.click()
 
     scrappy.set_web_driver(driver)
-    data = scrappy.scrape()
-    json_data = json.dumps(data)
-    print(json_data)
+    crawler.set_web_driver(driver)
+    # data = scrappy.scrape()
+    data = crawler.crawl_and_scrape(scrappy)
+
+    for water_system in data:
+        json_data = json.dumps(data)
+        print(json_data)
 
     driver.close()
 
