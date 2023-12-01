ad80beb3bf0aa1b2a9248438b4ecf7c1f98d3c70
RootCellar <RootCellar9877@gmail.com>
2023-11-30 16:01:21 -0900

    delete dead code

--- src/Crawler.py
+++ src/Crawler.py
@@ -51,57 +51,6 @@ class Crawler(object):
     def back_to_beginning(self):
         self.current_element = self.webdriver.find_element(By.TAG_NAME, "html")
 
-    @staticmethod
-    def is_after(element, other_element):
-        if other_element.location.get('y') < element.location.get('y'):
-            return False
-        if other_element.location.get('y') > element.location.get('y'):
-            return True
-        if other_element.location.get('x') > element.location.get('x'):
-            return True
-
-    @staticmethod
-    def is_before(element, other_element):
-        if other_element.location.get('y') > element.location.get('y'):
-            return False
-        if other_element.location.get('y') < element.location.get('y'):
-            return True
-        if other_element.location.get('x') < element.location.get('x'):
-            return True
-
-    def next_closest_element_in_list(self, elems):
-        self.__debug("next_closest_element_in_list")
-        closest = self.current_element
-        for elem in elems:
-            if self.is_after(self.current_element, elem):
-                closest = elem
-        for elem in elems:
-            if self.is_after(self.current_element, elem) and self.is_before(closest, elem):
-                closest = elem
-        return closest
-
-    def next_closest_element_in_list_with_attribute_and_value(self, elems, attribute, value):
-        self.__debug("next_closest_element_in_list_with_attribute_and_value")
-        closest = self.current_element
-        for elem in elems:
-            if self.is_after(self.current_element, elem):
-                closest = elem
-
-        if attribute is None or value is None:
-            for elem in elems:
-                if self.is_after(self.current_element, elem) and self.is_before(closest, elem):
-                    closest = elem
-        else:
-            for elem in elems:
-                if self.is_after(self.current_element, elem) and self.is_before(closest, elem) and elem.get_attribute(attribute) == value:
-                    closest = elem
-
-            if closest.get_attribute(attribute) != value:
-                # Didn't really find a match - set back to current element before return
-                closest = self.current_element
-        return closest
-
-
     def set_current_element(self, element):
         self.current_element = element
         self.__debug("Current element is now at " + self.current_element.location.__str__())
