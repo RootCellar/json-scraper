19fa82a18f7914361723137e4168a97c9233a5d0
RootCellar <RootCellar9877@gmail.com>
2023-11-30 20:49:56 -0900

    change should_print_debug to False, implement dump_stack()

--- src/Debug.py
+++ src/Debug.py
@@ -3,8 +3,21 @@
 #
 #
 
-should_print_debug = True
+should_print_debug = False
+
+import inspect
 
 def debug(string):
     if should_print_debug:
         print("[DEBUG] " + string)
+
+def dump_stack():
+    if should_print_debug:
+        stack = inspect.stack()
+        for stack_frame in stack:
+            frame = stack_frame[0]
+            info = inspect.getframeinfo(frame)
+
+            frame_string = info.function + ":" + info.lineno.__str__()
+
+            debug("at " + frame_string)
