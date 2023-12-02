44b7b7d88cfd783dd43040c018755603ef2818b7
RootCellar <RootCellar9877@gmail.com>
2023-12-01 20:49:43 -0900

    create a not-yet-working section for the sample results page

--- src/ScraperDefinitions/WaterSystem.py
+++ src/ScraperDefinitions/WaterSystem.py
@@ -58,6 +58,13 @@ def setup_scraper(scraper):
     scraper.then_scrape_table("facilities")
     scraper.then_go_back()
 
+    # scraper.then_skip_to_element_with_attribute("button", "value", "Coliform/Microbial Sample Results")
+    # scraper.then_click_element()
+    # scraper.then_skip_to_element("table")
+    # scraper.then_skip_to_element("table")
+    # scraper.then_scrape_table("sample_results")
+    # scraper.then_go_back()
+
 def setup_crawler(crawler):
 
     # The element that holds all of the item elements we really want
