377ce6d2e017194ddd9249e5af0e0f2065511a9c
RootCellar <RootCellar9877@gmail.com>
2023-12-05 19:46:47 -0900

    create instruction for saving an attribute value as a property

--- src/ScraperInstructionType.py
+++ src/ScraperInstructionType.py
@@ -9,7 +9,7 @@ class ScraperInstructionType(Enum):
     skip_to_tag = 1
     skip_to_class = 2
     save_value_as_property = 3
-    save_href_as_property = 4
+    save_attribute_as_property = 4
     back_to_beginning = 5
     skip_to_element_with_attribute = 6
     click_element = 7
